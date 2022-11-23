0092c632acea79397e7cc14c14440dad5dd13124
Sarvar01e <sernazarov185@gmail.com>
2022-11-23 15:10:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hw6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the beginning of the range");
+
+            int begin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the end of the range");
+
+            int end = int.Parse(Console.ReadLine());
+
+            while (begin < end)
+            {
+                begin++;
+                if (begin % 2 ==0)
+                {
+                    Console.WriteLine("an even number");
+                    Console.WriteLine(begin);
+                }
+                else
+                {
+                    Console.WriteLine("odd number");
+                    Console.WriteLine(begin);
+                }
+            }
+        }
+    }
+}
